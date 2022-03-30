f797ef1dcf975f1a9f3ddb3c1d2f4105b367ee90
brookbeyene <brookbeyene37@gmail.com>
2022-03-30 10:08:09 -0600

    Added InventoryControllerTest.java Tookout motor from url path

--- src/main/java/com/example/inventory/InventoryApplication.java
+++ src/main/java/com/example/inventory/InventoryApplication.java
@@ -2,6 +2,10 @@ package com.example.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class InventoryApplication {
@@ -9,5 +13,20 @@ public class InventoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
     }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/*")
+//                        .allowedMethods("")
+//                        .allowedOrigins("https://finalprojectdakota.web.app/")
+//                                .allowedHeaders("*")
+//                                .allowCredentials(false)
+//                                .maxAge(-1);
+//            }
+//        };
+//    }
 
 }
--- src/main/java/com/example/inventory/rest/Controller/InventoryController.java
+++ src/main/java/com/example/inventory/rest/Controller/InventoryController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/motor")
+@RequestMapping("/")
 public class InventoryController {
     private final InventoryRepository inventoryRepository;
 
